693b8982035e971ebb626214fe92f5aa4befc385
Aaron Goulden <aarong11@gmail.com>
2017-03-01 12:24:15 +0000

    Latest version

--- gamestate_integration.js
+++ gamestate_integration.js
@@ -4,7 +4,7 @@ var d2gsi = require('dota2-gsi');
 var socket = require('socket.io-client')('http://' + host + ':' + remoteEmitterPort);
 
 var server = new d2gsi({
-    port: 4000
+    port: 443
 });
 
 
@@ -17,13 +17,80 @@ server.events.on('newclient', function(client) {
 
       client.on('newdata', function(data) {
 
+	// If there are multiple players data, split the packets.
+	if(client.gamestate.player.team2)
+	{
+		let direPlayers = client.gamestate.player.team2;
+		let radiantPlayers = client.gamestate.player.team3;
+
+		let keys = Object.keys(direPlayers);
+
+		for(i = 0; i < keys.length; i++)
+		{
+	
+			console.log(client.gamestate.player.team2[keys[i]]);
+
+	
+        		let payload = {
+         			"partyId" : data.partyId,
+         			"playerToken": client.gamestate.player.team2[keys[i]].name,
+          			"payload": {
+					"gamestate": {
+						"player": client.gamestate.player.team2[keys[i]],
+						"hero": client.gamestate.hero.team2[keys[i]],
+						"items": client.gamestate.items.team2[keys[i]],
+						"abilities": client.gamestate.abilities.team2[keys[i]],
+						"buildings": client.gamestate.buildings
+					}
+				}
+        		};
+
+			socket.emit(partyId, payload);
+
+			//client.gamestate.hero = client.gamestate.hero.team2[keys[i]];
+		}
+
+
+		keys = Object.keys(radiantPlayers);
+		for(i = 0; i < keys.length; i++)
+		{
+	
+			console.log(client.gamestate.player.team3[keys[i]]);
+
+	
+        		let payload = {
+         			"partyId" : data.partyId,
+         			"playerToken": client.gamestate.player.team3[keys[i]].name,
+          			"payload": {
+					"gamestate": {
+						"player": client.gamestate.player.team3[keys[i]],
+						"hero": client.gamestate.hero.team3[keys[i]],
+						"items": client.gamestate.items.team3[keys[i]],
+						"abilities": client.gamestate.abilities.team3[keys[i]],
+						"buildings": client.gamestate.buildings
+					}
+				}
+        		};
+
+			socket.emit(partyId, payload);
+
+			//client.gamestate.hero = client.gamestate.hero.team3[keys[i]];
+		}
+
+	}
+
+	else
+	{
         let payload = {
           "partyId" : partyId,
           "playerToken": playerToken,
           "payload": client
         };
 
+
+
         socket.emit(partyId, payload);
+	}
       });
 
 });
